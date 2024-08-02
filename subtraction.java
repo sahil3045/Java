0453535c09a69d026c437e00315eb98f05be5e12
sahil3045 <sahilkanchan3045@gmail.com>
2024-08-02 18:12:31 +0530

    Create subtraction.java

---
+++ subtraction.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class subtraction {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the first number: ");
+        int x = sc.nextInt();
+        System.out.print("Enter the second number: ");
+        int y = sc.nextInt();
+        System.out.print("The difference of numner1 and number2 is: " + (x-y));
+
+
+    }
+    
+}
