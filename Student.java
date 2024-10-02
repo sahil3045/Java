56a9c530c60d2bf363541681005dfc825826edfb
sahil3045 <sahilkanchan3045@gmail.com>
2024-10-02 13:35:17 +0530

    Update Student.java

--- Student.java
+++ Student.java
@@ -3,53 +3,64 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.Scanner;
 
-// Class to store Student details
-class Student {
-    int id;
-    String name;
-    String studentClass;
+class student {
+  int id;
+  String name;
+  String Division;
 
-    // Constructor
-    public Student(int id, String name, String studentClass) {
-        this.id = id;
-        this.name = name;
-        this.studentClass = studentClass;
-    }
 
-    // Method to display student details
-    public void display() {
-        System.out.println("ID: " + id + ", Name: " + name + ", Class: " + studentClass);
-    }
+  public student(int id, String name, String Division) {
+    this.id = id;
+    this.name = name;
+    this.Division = Division;
+
+  }
+
+  public void display() {
+    System.out.println("Id: " + id + "Name: " + name + "Division: " + Division);
+
+  }
 }
 
-public class Main {
+  public class main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        ArrayList<Student> students = new ArrayList<>();
+      Scanner sc = new Scanner(System.in);
+      ArrayList<student> students = new ArrayList<>();
 
-        System.out.print("Enter number of students: ");
-        int numberOfStudents = scanner.nextInt();
+      System.out.println("Enter the number of students: ");
+      int noofstudents = sc.nextInt();
 
-        // Input student details
-        for (int i = 0; i < numberOfStudents; i++) {
-            System.out.println("Enter details for student " + (i + 1) + ":");
+      for (int i = 0; i < noofstudents; i++) {
+        System.out.println("Enter the student details of" + (i + 1) + ": ");
 
-            System.out.print("ID: ");
-            int id = scanner.nextInt();
-            scanner.nextLine();  // Consume newline left by nextInt()
+        System.out.print("Id: ");
+        int id = sc.nextInt();
 
-            System.out.print("Name: ");
-            String name = scanner.nextLine();
+        System.out.print("Name: ");
+        String name = sc.nextLine();
 
-            System.out.print("Class: ");
-            String studentClass = scanner.nextLine();
+        System.out.print("Division: ");
+        String Division = sc.nextLine();
 
-            // Add student to the list
-            students.add(new Student(id, name, studentClass));
+        students.add(new student(id, name, Division));
+      
+
+      }
+        Collections.sort(students, new Comparator<Student>() {
+          @Override
+          public int compare(Student s1, Student s2) {
+              return Integer.compare(s1.id, s2.id);
+          }
+        });
+
+        // Display sorted student details
+        System.out.println("\nStudents sorted by ID:");
+        for (Student student : students) {
+          student.display();
         }
+      Scanner.close()
 
-        
-
-        scanner.close();
+    
+    
     }
 }
