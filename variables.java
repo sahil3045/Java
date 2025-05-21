2d7de0432dd94cc89fea557c312f8dfa249df992
Sahil Kanchan <sahilkanchan3045@gmail.com>
2025-05-21 13:25:37 +0530

    Create variables.java

---
+++ variables.java
@@ -0,0 +1,22 @@
+public class variables {
+    public static void main(String[] args) {
+        int mynum;
+        mynum = 27;
+        System.out.println(mynum);
+
+        double mydoub = 7.23;
+        System.out.println(mydoub);
+
+        float myflo = 78.9f;
+        System.out.println(myflo);
+
+        char alpha = 'S';
+        System.out.println(alpha);
+
+        boolean mybool = true;
+        System.out.println(mybool);
+
+        byte mybyte = 127 ;
+        System.out.println(mybyte);
+    }
+}
