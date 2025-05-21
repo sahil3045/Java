581e941294ad16023856ca3f12cee5e97cb1c775
Sahil Kanchan <sahilkanchan3045@gmail.com>
2025-05-21 13:25:34 +0530

    Create string.java

---
+++ string.java
@@ -0,0 +1,14 @@
+public class string {
+    public static void main(String[] args) {
+        int myint = 7;
+        String text = "Hello"; 
+        String blank = " ";
+        String myname = "Sahil";
+
+        System.out.println(text + blank + myname);
+
+        System.out.println("The number is: " + myint);
+        
+
+    }
+}
