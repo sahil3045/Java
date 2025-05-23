c19f9a917c0f043b7515c3ed9a56ae4ff65ac91a
Sahil Kanchan <sahilkanchan3045@gmail.com>
2025-05-23 11:13:13 +0530

    Create For_loop.java

---
+++ For_loop.java
@@ -0,0 +1,8 @@
+public class For_loop {
+    public static void main(String[] args) {
+        for(int i=0; i<5; i++){
+            System.out.println("The value of i is: " +  i);
+        }
+    }
+    
+}
