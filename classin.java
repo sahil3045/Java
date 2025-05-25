fe11737464414469a1effdd56a8d2dcd63ab8a97
Sahil Kanchan <sahilkanchan3045@gmail.com>
2025-05-25 12:48:26 +0530

    Create classin.java

---
+++ classin.java
@@ -0,0 +1,24 @@
+class Person{
+    // instance (data or state)
+    String name;
+    int age;
+
+    // classes can contain 
+    //1) data 
+    //2) subroutines(methods)
+}
+public class classin {
+    public static void main(String[] args) {
+        Person person1 = new Person();
+    person1.name = "Manas";
+    person1.age = 16; 
+
+    Person person2 = new Person();
+    person2.name = "Sahil";
+    person2.age = 20;
+
+    System.out.println(person1.name);
+    }
+
+    
+}
