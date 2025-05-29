67e422f469522a1f70d673dfa97833b4e930848d
Sahil Kanchan <sahilkanchan3045@gmail.com>
2025-05-29 13:10:30 +0530

    Update ENCAPSULE.java

---
+++ ENCAPSULE.java
@@ -0,0 +1,17 @@
+class Plant{
+    private string name;
+
+    public string getname(){
+        return name;
+    }
+
+    public void setname(String name){ 
+        this.name = name;
+    }
+}
+public class ENCAPSULE {
+    public static void main(String[] args) {
+        
+    }
+    
+}
