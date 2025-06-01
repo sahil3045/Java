2900dd55e0bf893ecdd5734ac42cb437d2eba391
Sahil Kanchan <sahilkanchan3045@gmail.com>
2025-06-01 12:25:44 +0530

    Update handle_exceptions.java

---
+++ handle_exceptions.java
@@ -0,0 +1,16 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+public class handle_exceptions {
+    public static void main(String[] args) {
+        File file = new File("test.txt");
+        try{
+            FileReader fr = new FileReader(file);
+        } catch(FileNotFoundException e) {
+            e.printStackTrace(); 
+
+        }
+
+    }
+    
+}
