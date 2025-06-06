963a4378943c468f67a8c639b16d9603d34e244d
Sahil Kanchan <sahilkanchan3045@gmail.com>
2025-06-06 11:07:25 +0530

    Create innercls.java

---
+++ innercls.java
@@ -0,0 +1,28 @@
+class Robot23{
+    private int id;
+
+    // Inner Class!
+    class Brain{
+        public void think(){
+            System.out.println("Robot " + id + " is thinking.");
+        }
+    }
+
+    public Robot23(int id){
+        this.id = id;
+    }
+
+    public void start(){
+        System.out.println("Robot starting " + id);
+
+        Brain brain = new Brain(); 
+        brain.think();
+    }
+}
+public class innercls {
+    public static void main(String[] args) {
+        Robot23 robot1 = new Robot23(8);
+        robot1.start();
+    }
+    
+}
