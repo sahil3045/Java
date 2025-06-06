e41d807803f77aaf2f3862223f09fa7e78e56606
Sahil Kanchan <sahilkanchan3045@gmail.com>
2025-06-06 11:36:37 +0530

    Create enum_type.java

---
+++ enum_type.java
@@ -0,0 +1,20 @@
+
+public class enum_type {
+    public static final int DOG = 1;
+    public static final int CAT = 3;
+    public static final int MOUSE = 5;
+    public static void main(String[] args) {
+        Animal animal = new Animal.DOG;
+        switch (animal){
+            case DOG:
+                System.out.println("Dog");
+                break;
+            case CAT:
+                System.out.println("Cat");
+                break;
+            case MOUSE:
+                System.out.println("Mouse");
+                break;
+        }
+    }
+}
