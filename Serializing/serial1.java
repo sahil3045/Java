8b5e876d301d9de53ec5d6c650d1547fae905e91
Sahil Kanchan <sahilkanchan3045@gmail.com>
2025-06-09 12:23:59 +0530

    Update serial1.java

---
+++ Serializing/serial1.java
@@ -0,0 +1,9 @@
+package Serializing;
+
+public class serial1 {
+    public static void main(String[] args) {
+        System.out.println("Writing objects! ");
+
+        Person new person()
+    }
+}
