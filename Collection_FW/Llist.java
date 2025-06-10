02b53aade144be44e57c524264f0dfa9e733c44c
Sahil Kanchan <sahilkanchan3045@gmail.com>
2025-06-10 10:35:03 +0530

    Update Llist.java

--- Collection_FW/Llist.java
+++ Collection_FW/Llist.java
@@ -5,7 +5,27 @@ import java.util.List;
 
 public class Llist {
     private static void doTimings(String type, List<Integer> list){
+         for(int i = 0; i < 1E5; i++){
+            list.add(i);
+        }
          
+    
+        long start = System.currentTimeMillis();
+        /*
+        // add items at end of list 
+        for(int i = 0; i <1E5; i++){
+            list.add(i);
+        }
+        */
+        /*
+         * add items elsewhere in the list 
+         */
+        for(int i = 0; i<1E5; i++){
+            list.add(0, i);
+        }
+         
+        long end = System.currentTimeMillis();
+        System.out.println("Time taken: " + (end-start) + " ms for " + type );
     }
     public static void main(String[] args) {
         ArrayList<Integer> arraylist = new ArrayList<Integer>();
@@ -15,5 +35,6 @@ public class Llist {
         doTimings("LinkedList", linkedlist);
 
     }
+    
     
 }
