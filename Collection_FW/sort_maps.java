842b2077ef4334bd521674853f4cabc13233f55b
Sahil Kanchan <sahilkanchan3045@gmail.com>
2025-06-11 11:05:59 +0530

    Create sort_maps.java

---
+++ Collection_FW/sort_maps.java
@@ -0,0 +1,21 @@
+package Collection_FW;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.TreeMap;
+
+
+class Temp{
+
+}
+
+public class sort_maps {
+    public static void main(String[] args) {
+        HashMap<Integer, String> hashmap = new HashMap<Integer, String>();
+        // linked hashmap uses double linkedlist.
+        LinkedHashMap<Integer, String> linkedHashMap = new LinkedHashMap<Integer, String>();
+        TreeMap<Integer, String> treehashmap = new TreeMap<Integer, String>();
+
+        System.out.println(new Temp());
+    }
+    
+}
