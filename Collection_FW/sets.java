4d0bd07a78ea570d945fc85ba7939e96d743e929
Sahil Kanchan <sahilkanchan3045@gmail.com>
2025-06-12 11:18:35 +0530

    Update sets.java

--- Collection_FW/sets.java
+++ Collection_FW/sets.java
@@ -1,9 +1,12 @@
 package Collection_FW;
 import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.Set;
 
 public class sets {
     public static void main(String[] args) {
+        //Hashset does not remember order in which items are added
+    
         Set<String> set1 = new HashSet<String>();
         set1.add("Dog");
         set1.add("Cat");
@@ -11,8 +14,14 @@ public class sets {
 
         //adding dubplicate ietms has no effect 
         set1.add("Mouse");
-
         System.out.println(set1);
+
+         //Linkedhashset remembers the order you added the items in
+        Set<String> lset = new LinkedHashSet<String>();
+        lset.add("Sam");
+        lset.add("Porter");
+        lset.add("Bridges");
+        System.out.println(lset);
 
 
     }
