3871b50b3ed7ebb080b5320ea53807330df8a68e
Sahil Kanchan <sahilkanchan3045@gmail.com>
2025-06-12 11:01:56 +0530

    Update sets.java

--- Collection_FW/sets.java
+++ Collection_FW/sets.java
@@ -1,8 +1,20 @@
 package Collection_FW;
+import java.util.HashSet;
+import java.util.Set;
 
 public class sets {
     public static void main(String[] args) {
-        Set<>
+        Set<String> set1 = new HashSet<String>();
+        set1.add("Dog");
+        set1.add("Cat");
+        set1.add("Mouse");
+
+        //adding dubplicate ietms has no effect 
+        set1.add("Mouse");
+
+        System.out.println(set1);
+
+
     }
     
 }
