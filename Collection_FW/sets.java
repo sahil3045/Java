dc327ee4b75091673edd9956d78254bcdb11f1fc
Sahil Kanchan <sahilkanchan3045@gmail.com>
2025-06-15 12:02:09 +0530

    Update sets.java

--- Collection_FW/sets.java
+++ Collection_FW/sets.java
@@ -26,8 +26,31 @@ public class sets {
 
         //Treeset sorts in natural order;
         Set<String> set2 = new TreeSet<String>();
+        set2.add("sam");
+        set2.add("Hardman");
+        set2.add("Cat");
         
+        ///////Iteration////
+        for(String element: set1){
+            System.out.println(element);
+        }
 
+        /// Does set contain any given item?
+        if(set1.contains("Cat")){
+            System.out.println("Contains Cat");
+        }
+
+        //Interscetion 
+        //create a new duplicate set 
+        Set<String> intersection = new HashSet<String>(set1);
+        intersection.retainAll(set2);
+        System.out.println(intersection);
+
+        //Difference
+        Set<String> diff = new HashSet<String>(set1);
+        diff.removeAll(set2);
+        System.out.println(diff);
+        
 
     }
     
